75ffb3830dec662e22249c0f6f2c9ef7f7394aa3
zzzwco <zzzwco@outlook.com>
2022-07-13 10:28:07 +0800

    Update Examples

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -6,21 +6,18 @@
 //
 
 import UIKit
-import SwiftMessages
 
 class ViewController: UIViewController {
   
   private let container = KZFlexView(axis: .vertical)
-  private let title1L = titleLClousure("iPhone、iPad")
-  private let title2L = titleLClousure("Mac")
+  private let title1L = createTitle("iPhone、iPad")
+  private let title2L = createTitle("Mac")
   private let iphonesView = KZFlexView(axis: .horizontal)
   private var isiPhonesViewHidden = false
   
-  private static let titleLClousure: (String) -> UILabel = { title -> UILabel in
-    UILabel().kz.with { v in
-      v.font = .kz.size(20).kz.weight(.bold)
-      v.text = title
-    }
+  private static let createTitle: (String) -> UILabel = { title -> UILabel in
+    UILabel().kz.text(title)
+      .kz.font(UIFont.kz.size(20).kz.weight(.bold))
   }
   
   // MARK: - Life cycle
@@ -36,30 +33,29 @@ class ViewController: UIViewController {
     navigationItem.leftBarButtonItem = UIBarButtonItem(
       barButtonSystemItem: .play, target: self, action: #selector(tmp))
     
-    container.margins(.init(top: 20, left: 20, bottom: 20, right: 20)).spacing(20)
-      .kz.addToView(view, constraints: { make in
-        make.edges.equalToSuperview()
-      })
-      .addArrangedSubview(title1L)
-      .addArrangedSubview(iphonesView, constraints: { make in
-        make.height.equalTo(view.snp.width).dividedBy(2.5)
-      })
-      .addArrangedSubview(title2L)
-      .addArrangedSubviews(Product.macs.map { ProductView(.mac).refreshUI(with: $0) })
+    container.kz.addToView(view, constraints: { make in
+      make.edges.equalToSuperview()
+    })
+    .margins(.init(top: 20, left: 20, bottom: 20, right: 20)).spacing(20)
+    .addArrangedSubview(title1L)
+    .addArrangedSubview(iphonesView, constraints: { make in
+      make.height.equalTo(view.snp.width).dividedBy(2.5)
+    })
+    .addArrangedSubview(title2L)
+    .addArrangedSubviews(Product.macs.map { ProductView(.mac).refreshUI(with: $0) })
     
-    iphonesView.spacing(15)
-      .kz.with { v in
-        v.showsHorizontalScrollIndicator = false
-        v.layer.masksToBounds = false
-        
-        for p in Product.iPhones {
-          let iPhoneView = ProductView(.iPhone).refreshUI(with: p)
-          v.addArrangedSubview(iPhoneView)
-          iPhoneView.kz.makeConstraints { make in
-            make.width.equalTo(iPhoneView.snp.height)
-          }
+    iphonesView.spacing(15).kz.with { v in
+      v.showsHorizontalScrollIndicator = false
+      v.layer.masksToBounds = false
+      
+      for p in Product.iPhones {
+        let iPhoneView = ProductView(.iPhone).refreshUI(with: p)
+        v.addArrangedSubview(iPhoneView)
+        iPhoneView.kz.makeConstraints { make in
+          make.width.equalTo(iPhoneView.snp.height)
         }
       }
+    }
   }
 }
 
